030a207f0c0bb520d5bc71b4387556c3979d975b
nmn <namanagg.2010@gmail.com>
2021-09-15 22:26:53 +0530

    commit

--- foodApp/server.js
+++ foodApp/server.js
@@ -1,4 +1,5 @@
 const express=require('express');
+
 const app=express();
 // const router=express.Router();
 app.listen('5000',function(){
@@ -6,10 +7,21 @@ app.listen('5000',function(){
 });
 
 app.use(express.json());
+// app.use((req,res,next)=>{
+//     //do some work
+//     console.log('i am a middleware');
+//     next();
+// });
 
+app.use(express.static('public'));
 const userRouter=express.Router();
+const authRouter=express.Router();
+
 app.use('/user',userRouter);
+app.use('/auth',authRouter);
 //mounting in express
+
+
 userRouter
 .route('/')
 .get(getUser)
@@ -17,22 +29,94 @@ userRouter
 .patch(updateUser)
 .delete(deleteUser);
 
+// app.use((req,res,next)=>{
+//     //do some work
+//     console.log('i am a middleware 2nd time');
+//     next();
+// });
+
 userRouter
 .route('/:id')
 .get(getUserById);
 
+authRouter
+.route('/signup')
+.post(signupUser);
 
-let user={};
+authRouter
+.route('/forgetPassword')
+.get(getForgetPassword)
+.post(postForgetPassword,validateEmail);
+
+function getForgetPassword(req,res){
+    res.sendFile('./public/forgetPassword.html',{root:__dirname});
+}
+
+function postForgetPassword(req,res,next){
+    let data=req.body;
+    console.log('data',data);
+    //check if email id is correct- validate
+    next();
+    //check if user exists in db
+    // res.json({
+    //     message:"data received",
+    //     data:data.email
+    // })
+};
+
+function validateEmail(req,res){
+    console.log('in validateEmail function');
+    console.log(req.body);
+    //hw to check if email is correct or not -> @ , .
+    //indexOf
+     res.json({
+            message:"data received",
+            data:req.body
+        });
+}
+
+
+https://classroom.pepcoding.com/index
+//redirects
+app.get('/user-all',(req,res)=>{
+    res.redirect('/user');
+});
+
+//404 page
+app.use((req,res)=>{
+    res.sendFile('public/404.html',{root:__dirname})
+});
+
+
+
+function signupUser(req,res){
+    // let userDetails=req.body;
+    // let name=userDetails.name;
+    // let email=userDetails.email;
+    // let password=userDetails.password;
+
+    let{email,name,password}=req.body;
+    user.push({email,name,password});
+    console.log('user',req.body);
+    res.json({
+        message:'user signedUp',
+        user:req.body
+    });
+}
+
+
+let user=[];
 // client <- server
 //crud- create read update delete
 //read
-app.get('/',(req,res)=>{
-    res.send('Home Page');
-});
+// app.get('/',(req,res)=>{
+//     res.send('Home Page');
+// });
 
 // app.get('/user',getUser);
 
 function getUser(req,res){
+    console.log('getUser called');
     res.json(user);
 }
 
